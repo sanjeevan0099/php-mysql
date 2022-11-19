dcec89ada3d8dc790548ac2287fbd0d334a4d952
Sanjeevan N Gowda <100146349+sanjeevan0099@users.noreply.github.com>
2022-11-18 22:54:33 -0800

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'database-1.cd76xrvgvxym.ap-south-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'sanju0099');
-define('DB_NAME', 'emp_db');    
+define('DB_NAME', 'employees_db');    
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
